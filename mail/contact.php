d7035abef0d27c605173e98f3a10dde1982913ab
Chaudhary Sachin Kumar <chsachinkumar2003@gmail.com>
2022-12-17 20:24:23 +0530

    Update

---
+++ mail/contact.php
@@ -0,0 +1,31 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "techingrow@gmail.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+<<<<<<< Updated upstream
+
+=======
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+>>>>>>> Stashed changes
+  
+if($email!=NULL)
+{
+  mail($to,$subject,$text,$headers);
+}
+header("Location:www.shivinsurance.live");
+
+?>
